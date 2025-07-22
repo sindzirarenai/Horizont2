9c8b01cdd75d48c824148e7fee851b8d633d3b4a
WORK\alfiya.arshina <sindzirarenai@gmail.com>
2025-07-22 15:47:12 +0500

    initial commit

---
+++ Horizont2/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Horizont.Connection;
+using Horizont.Models;
+using Horizont.Services;
+
+namespace Horizont2.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+
+        private readonly ILogger<HomeController> _logger;
+        private ISaleService SaleService { get; set; }
+        private IBaseRepository<Contrpartner> Contrpartners { get; set; }
+        private IBaseRepository<Assortment> Assortments { get; set; }
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public JsonResult GetContrpartners()
+        {
+            return new JsonResult(Contrpartners.GetAll());
+        }
+
+        [HttpGet]
+        public JsonResult GetAssortments()
+        {
+            return new JsonResult(Assortments.GetAll());
+        }
+
+        [HttpGet]
+        public JsonResult GetAssortmentApriori(List<long> ids)
+        {
+            return new JsonResult(null); //return new JsonResult(SaleService.GetAprioriAssortment(ids));
+        }
+
+    }
+}
---
+++ Horizont2/Services/SaleService.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using Horizont.Connection;
+using Horizont.Models;
+
+namespace Horizont.Services
+{
+    public class SaleService:ISaleService
+    {
+       private IBaseRepository<Sale> Sales { get; set; }
+        private IBaseRepository<SaleDocument> SaleDocuments { get; set; }
+        private IBaseRepository<Contrpartner> Contrpartners { get; set; }
+        private IBaseRepository<Assortment> Assortments { get; set; }
+
+
+
+        public List<Contrpartner> GetContrpartners()
+        {
+            return Contrpartners.GetAll();
+        }
+
+  
+
+        public List<SaleDocument> GetSaleDocumentsByContrpartner(Contrpartner contrpartner)
+        {
+            return contrpartner.SaleDocuments.ToList();
+        }
+
+        /*public List<Assortment> GetAprioriAssortment(List<long> ids)
+        {
+            var dictionary = SaleDocuments.Linq()
+                .Where(x => x.GetAssortments().Count(y => ids.Contains(y.Id)) == ids.Count)
+                .ToDictionary(y => y, x => x.Sales.Select(z => z.Assortment).OrderBy(z => z.Id).ToList());
+            var uniqueAssortments =
+                dictionary.SelectMany(x => x.Value).Distinct().Where(x => !ids.Contains(x.Id)).ToList();
+
+            var supportDictionary = new Dictionary<Assortment, int>();
+            foreach (var uniqueAssortment in uniqueAssortments)
+            {
+                supportDictionary.Add(uniqueAssortment, dictionary.Where(x=>x.Value.Contains()));   
+            }
+        }*/
+    }
+}
